9491c63b917c81a650e23b13462c0c0c6d0711c9
Florin Niculescu <florin.niculescu999@gmail.com>
2022-02-07 21:40:33 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+
+    } else {
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * parseInt(years) * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result_line1 = "If you deposit <mark>" + principal + "</mark>, \<br\>";
+    var result_line2 = "at an interest rate of <mark>" + rate + "%" + "</mark>. \<br\>";
+    var result_line3 = "You will receive an amount of <mark>" + interest + "</mark>, \<br\>";
+    var result_line4 = "in the year <mark>" + year + "</mark> \<br\>";
+    document.getElementById("result").innerHTML = result_line1 + result_line2 + result_line3 + result_line4; 
+    }
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
